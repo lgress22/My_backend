3af3d52b42a9f5031d68bd171a45edd5ee633681
liam gress <gress_l@qwasar.io>
2023-01-06 19:02:05 +0000

    My Backend

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-// const express = require("express");
-// const app = express();
-// const port = 8080;
+const express = require("express");
+const app = express();
+const port = 8080;
 
 
-// const songs = ["New York, New York", "That's Life", "Fly Me to the Moon", "My Way", "Jingle Bells" , "Come Fly with Me", "Somehthin' Stupid", 
-// "Summer Wind", "I've got You Under My Skin", "Luck Be A Ladt", "I'm a Fool to Want You", "Let It Snow! Let It Snow! Let It Snow!", 
-// "Santa Claus Is Coming To Town", "Everybody Loves Somebody", "Let Me Try Again", "It Had To Be You", "Silent Night, Holy Night",
-//  "Mistletoe and Holly", "You Make Me Feel so Young"]
+const songs = ["New York, New York", "That's Life", "Fly Me to the Moon", "My Way", "Jingle Bells" , "Come Fly with Me", "Somehthin' Stupid", 
+"Summer Wind", "I've got You Under My Skin", "Luck Be A Lady", "I'm a Fool to Want You", "Let It Snow! Let It Snow! Let It Snow!", 
+"Santa Claus Is Coming To Town", "Everybody Loves Somebody", "Let Me Try Again", "It Had To Be You", "Silent Night, Holy Night",
+ "Mistletoe and Holly", "You Make Me Feel so Young"]
 
-// app.get("/", (req, res) => {
-//     console.log("Here route ROOT")
-//     res.send(songs)
-// })
+ const wives = ("Nancy Barbato, Ava Gardner, Mia Farrow, Barbara Marx")
 
-// app.get("/Liam", (req, res) => {
-//     res.send("Hello Gaetan")
-// })
+app.get("/", (req, res) => {
+    console.log("Here route ROOT")
+    const RandomSongs = songs[Math.floor(Math.random() * songs.length)]
+
+    res.send(RandomSongs)
+})
+
+app.get("/birthdate", (req, res) => {
+    res.send("December 12, 1915")
+})
+
+app.get("/birthcity", (req, res) => {
+    res.send("Hoboken, New Jersey")
+})
+
+app.get("/wives", (req, res) => {
+    res.send(wives)
+})
 
 
-// app.listen(port, (error) =>{
-//     if(!error)
-//     console.log("Server is Succesfully Running, and app is listening on port" + port)
-//     else
-//     console.log("Error occured, server can't start", error);
+app.get("/picture", (req,res) => {
+    res.redirect("https://en.wikipedia.org/wiki/Frank_Sinatra#/media/File:Frank_Sinatra2-_Pal_Joey.jpg")
+})
 
-// })
+app.listen(port, (error) =>{
+    if(!error)
+    console.log("Server is Succesfully Running, and app is listening on port" + port)
+    else
+    console.log("Error occured, server can't start", error);
+
+})
+
+
+console.log(songs)
