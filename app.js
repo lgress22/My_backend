3e616377b1bf675cd3904ddf66d3097c20cd648b
liam gress <gress_l@qwasar.io>
2023-01-06 19:59:25 +0000

    My Backend

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-
+const basicAuth = require("express-basic-auth")
 
 const songs = ["New York, New York", "That's Life", "Fly Me to the Moon", "My Way", "Jingle Bells" , "Come Fly with Me", "Somehthin' Stupid", 
 "Summer Wind", "I've got You Under My Skin", "Luck Be A Lady", "I'm a Fool to Want You", "Let It Snow! Let It Snow! Let It Snow!", 
@@ -17,11 +17,11 @@ app.get("/", (req, res) => {
     res.send(RandomSongs)
 })
 
-app.get("/birthdate", (req, res) => {
+app.get("/birth_date", (req, res) => {
     res.send("December 12, 1915")
 })
 
-app.get("/birthcity", (req, res) => {
+app.get("/birth_city", (req, res) => {
     res.send("Hoboken, New Jersey")
 })
 
@@ -33,6 +33,22 @@ app.get("/wives", (req, res) => {
 app.get("/picture", (req,res) => {
     res.redirect("https://en.wikipedia.org/wiki/Frank_Sinatra#/media/File:Frank_Sinatra2-_Pal_Joey.jpg")
 })
+
+
+app.get("/public", (req,res) => {
+    res.send("Everybody can see this page")
+})
+
+
+// app.use(basicAuth({
+//     users: {"admin": "admin"}
+// }))
+
+// app.get("/protected", (req, res) => {
+//     console.log("/user request called")
+//     res.send("Welcome, authenticated client")
+// })
+
 
 app.listen(port, (error) =>{
     if(!error)
