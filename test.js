79e2d1f25d6cace71163be1bdffc2ce8538b987a
liam gress <gress_l@qwasar.io>
2023-01-06 00:49:15 +0000

    My Backend

--- test.js
+++ test.js
@@ -1,16 +1,21 @@
+
+
 const express = require("express");
 const app = express();
 const port = 8080;
 
 
-songs = ["New York, New York", "That's Life", "Fly Me to the Moon", "My Way", "Jingle Bells" , "Come Fly with Me", "Somehthin' Stupid", 
-"Summer Wind", "I've got You Under My Skin", "Luck Be A Ladt", "I'm a Fool to Want You", "Let It Snow! Let It Snow! Let It Snow!", 
+const songs = ["New York, New York", "That's Life", "Fly Me to the Moon", "My Way", "Jingle Bells" , "Come Fly with Me", "Somehthin' Stupid", 
+"Summer Wind", "I've got You Under My Skin", "Luck Be A Lady", "I'm a Fool to Want You", "Let It Snow! Let It Snow! Let It Snow!", 
 "Santa Claus Is Coming To Town", "Everybody Loves Somebody", "Let Me Try Again", "It Had To Be You", "Silent Night, Holy Night",
  "Mistletoe and Holly", "You Make Me Feel so Young"]
 
+
 app.get("/", (req, res) => {
     console.log("Here route ROOT")
-    res.send("Hey John")
+    const RandomSongs = songs[Math.floor(Math.random() * songs.length)]
+
+    res.send({songs:RandomSongs})
 })
 
 app.get("/gtn", (req, res) => {
@@ -18,10 +23,13 @@ app.get("/gtn", (req, res) => {
 })
 
 
-// app.listen(port, '0.0.0.0', (error) =>{
-//     if(!error)
-//     console.log("Server is Succesfully Running, and app is listening on port" + port)
-//     else
-//     console.log("Error occured, server can't start", error);
+app.listen(port, (error) =>{
+    if(!error)
+    console.log("Server is Succesfully Running, and app is listening on port" + port)
+    else
+    console.log("Error occured, server can't start", error);
 
-// })
+})
+
+
+console.log(songs)
